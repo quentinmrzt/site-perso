bbdc0e32c960839850e0fe54229d0bbd96fc3195
quentinmrzt <quentin.morizot@outlook.fr>
2018-11-07 23:03:09 +0100

    Ajout d'une nouvelle couleure

--- src/app/cv/cv.directive.ts
+++ src/app/cv/cv.directive.ts
@@ -5,8 +5,10 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class BorderCardDirective {
 
+	private couleur08h15: string = "#F3C36E";
 	private couleur12h31: string = "#DBC195";
 
 	constructor(private el: ElementRef) {
+		
 	}
 }
