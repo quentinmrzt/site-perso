5d535eb46efd84641f0c13c5dd5054226a9f4869
quentinmrzt <quentin.morizot@outlook.fr>
2018-11-11 23:55:12 +0100

    Ajout d'une  horloge basique + correction des dates cathédrales

--- src/app/projets/sitePerso/sitePerso.component.ts
+++ src/app/projets/sitePerso/sitePerso.component.ts
@@ -15,6 +15,54 @@ export class ProjetSitePersoComponent implements OnInit {
 	constructor(private router: Router) { }
 
 	ngOnInit(): void {
-
+		this.initLocalClocks();
 	}
+
+
+
+	/*
+	 * Starts any clocks using the user's local time
+	 * From: cssanimation.rocks/clocks
+	 */
+	private initLocalClocks() {
+	  // Get the local time using JS
+	  var date = new Date;
+	  var seconds = date.getSeconds();
+	  var minutes = date.getMinutes();
+	  var hours = date.getHours();
+
+	  var aiguilles = [
+	    {
+	      aiguille: 'hours',
+	      angle: (hours * 30) + (minutes / 2)
+	    },
+	    {
+	      aiguille: 'minutes',
+	      angle: (minutes * 6)
+	    },
+	    {
+	      aiguille: 'seconds',
+	      angle: (seconds * 6)
+	    }
+	  ];
+
+	  for (var j = 0; j < aiguilles.length; j++) {
+	    var elements = document.querySelectorAll('.' + aiguilles[j].aiguille);
+	    for (var k = 0; k < elements.length; k++) {
+	        elements[k].style.webkitTransform = 'rotateZ('+ aiguilles[j].angle +'deg)';
+	        elements[k].style.transform = 'rotateZ('+ aiguilles[j].angle +'deg)';
+	        // If this is a minute aiguille, note the seconds position (to calculate minute position later)
+	        if (aiguilles[j].aiguille === 'minutes') {
+	          elements[k].parentNode.setAttribute('data-second-angle', aiguilles[j + 1].angle);
+	        }
+	    }
+	  }
+	}
+
+
+
+
+
+
+
 }
