4f825764f925e0b99eb1b5fb98ef443afe2a7bf3
quentinmrzt <quentin.morizot@outlook.fr>
2018-12-19 22:47:31 +0100

    Ajout du menu responsive (avec encore bcp de bug)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { slideInAnimation } from './animations';
@@ -7,13 +7,37 @@ import { slideInAnimation } from './animations';
   selector: 'site-perso',
   templateUrl: './app/app.component.html',
   styleUrls: ['./app/app.component.css'],
-	animations: [
+  animations: [
     slideInAnimation
     // animation triggers go here
   ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  menuOuvert: boolean = false;
+
+  ngOnInit(): void {
+    if(window.innerWidth >= 768) {
+      this.menuOuvert = true;
+    } else {
+      this.menuOuvert = false;
+    }
+  }
+
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
+
+  cliqueSurLeMenu() {
+    this.menuOuvert = !this.menuOuvert;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    if(window.innerWidth >= 768) {
+      this.menuOuvert = true;
+    } else {
+      this.menuOuvert = false;
+    }
   }
 }
