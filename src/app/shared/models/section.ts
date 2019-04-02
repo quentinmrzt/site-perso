5a4679bf99e576bf7395e11dc8d592aa9357ca83
quentinmrzt <quentin.morizot@outlook.fr>
2019-04-02 23:21:44 +0200

    Ajout des photos dans la partie formations

--- src/app/shared/models/section.ts
+++ src/app/shared/models/section.ts
@@ -74,21 +74,21 @@ export const SECTIONEXPERIENCES: Section[] = [
     date: "Depuis Septembre",
     lieu: "Efluid, Metz",
     resume: "",
-    detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nulla justo, porta id tempus in, pretium eget leo. Duis vulputate vitae elit et gravida. Aenean ultricies ante arcu, id condimentum."
+    detail: "Développeur Java au sein de l'équipe contrat sur le progiciel Efluid. Réalisation de correction de bug, maintenance de code et qualité, mise en place de tests unitaires, 100% coverage, et ajouts de nouvelles fonctionnalités."
   },
   {
     titre: "Employé commercial",
     date: "Août 2016",
     lieu: "Intermarché, Contrexéville",
     resume: "",
-    detail: "Gestion de plusieurs rayons: mise en place, gestion des stocks et approvisionnement."
+    detail: "Gestion de plusieurs rayons du magasin. Exécution de tâches diverses allant de la mise en place des marchandises, la gestion des stocks et l'approvisionnement de ceux-ci."
   },
   {
     titre: "Agent contractuel",
     date: "Juillet 2016",
     lieu: "Mairie - Espace Vert, Vittel",
     resume: "",
-    detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nulla justo, porta id tempus in, pretium eget leo. Duis vulputate vitae elit et gravida. Aenean ultricies ante arcu, id condimentum."
+    detail: ""
   },
   {
     titre: "Serveur / Barman",
