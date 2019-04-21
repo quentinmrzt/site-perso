e2495bfdfacbce55d3fdb995e0976961bc2f836e
quentinmrzt <quentin.morizot@outlook.fr>
2019-04-21 23:31:57 +0200

    Amélioration taille des images, alignement des titres pour la version mobile et modif mineur IHM

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -8,16 +8,20 @@ import { FormationsComponent } from './formations/formations.component';
 import { ProjetsComponent } from './projets/projets.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+import { SitePersoComponent } from './projets/site-perso/site-perso.component';
+import { PhotosDeCouvertureComponent } from './projets/photos-de-couverture/photos-de-couverture.component';
+
 //import { SitePersoComponent } from './projets/site-perso/site-perso.component';
 
 const routes: Routes = [
   { path: 'competences', component: CompetencesComponent, data: {animation: 'CompetencesPage'} },
-  { path: '', redirectTo: '/accueil', pathMatch: 'full' },
   { path: 'accueil', component: AccueilComponent, data: {animation: 'AccueilPage'} },
   { path: 'experiences', component: ExperiencesComponent, data: {animation: 'ExperiencesPage'} },
   { path: 'formations', component: FormationsComponent, data: {animation: 'FormationsPage'} },
   { path: 'projets', component: ProjetsComponent, data: {animation: 'ProjetsPage'} },
-  //{ path: 'projet', loadChildren: './projets/projets.module#ProjetsModule' },
+  { path: 'projet/siteperso', component: SitePersoComponent, data: {animation: 'SitePersoPage'} },
+  { path: 'projet/photosdecouverture', component: PhotosDeCouvertureComponent, data: {animation: 'PhotosDeCouverturePage'} },
+  { path: '', redirectTo: '/accueil', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
 
